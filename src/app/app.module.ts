1de36c612e12af40a90604a057c7ec659a926427
Marc Bo <m.bonacker@freenet.de>
2023-02-06 17:45:49 +0100

    Added spotify component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SpotifyComponent } from './spotify/spotify.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SpotifyComponent,
   ],
   imports: [
     BrowserModule,
